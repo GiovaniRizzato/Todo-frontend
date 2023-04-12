c756d1a3b8a29087fa9ce18867789928300d1678
Giovani Rizzato <giovanirizzato@gmail.com>
2023-04-12 00:02:32 -0300

    Styling

--- src/commom/text-list/text-list.module.ts
+++ src/commom/text-list/text-list.module.ts
@@ -4,6 +4,7 @@ import { FormsModule } from '@angular/forms';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatIconModule } from '@angular/material/icon'
 import { MatButtonModule } from '@angular/material/button'
+import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field'
 import { MatInputModule } from '@angular/material/input';
 import { TextItemComponent } from './text-item/text-item.component';
@@ -21,6 +22,7 @@ import { TextListComponent } from './text-list.component';
     MatButtonModule,
     MatIconModule,
     MatFormFieldModule,
+    MatCardModule,
     MatInputModule
   ],
   exports: [
