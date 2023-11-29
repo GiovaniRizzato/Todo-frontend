c121973e85404e7b522c1df46902bb8468ed7798
Giovani Rizzato <giovanirizzato@gmail.com>
2023-11-29 20:54:24 -0300

    Fix the unit testing

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,11 +1,25 @@
 import { render, screen } from '@testing-library/angular'
 import { AppComponent } from './app.component';
+import { TodoService } from './services/todo.service';
+import { Mock } from 'ts-mockery'
+import { of } from 'rxjs'
 
 describe('AppComponent', () => {
+  beforeEach(async () => {
+    const mockTodoService: TodoService = Mock.from<TodoService>({
+      getAllTodos: () => of([])
+    });
+
+    await render (AppComponent, {
+      providers: [
+        { provide: TodoService, useFactory: () => mockTodoService }
+      ],
+    });
+  });
+
   describe('When the page loads', () => {
-    it('Should render the message', async () => {
-      await render (AppComponent)
-      expect (screen.getByText('Todo-frontend app is running!')).toBeInTheDocument ();
+    it('Should render the message', () => {
+      expect (screen.getByText('Todo-frontend')).toBeInTheDocument ();
     });
   });
 });
