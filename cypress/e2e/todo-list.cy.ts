6041e7597d7b9f2ecbec67d1cfd01c43147ce95e
Giovani Rizzato <giovanirizzato@gmail.com>
2024-03-28 22:47:57 -0300

    Finishing the cypress jorney coverage for e2e

--- cypress/e2e/todo-list.cy.ts
+++ cypress/e2e/todo-list.cy.ts
@@ -41,5 +41,57 @@ describe('Todo list page', () => {
     after(() => {
       cy.mocksRestoreRouteVariants();
     });
-  })
+  });
+
+  describe('When the endpoints for modifying todos returns errors', () => {
+    beforeEach(() => {
+      cy.mocksSetCollection("error");
+      cy.mocksSetDelay(100);
+      cy.visit("/");
+      cy.wait(300);
+    });
+
+    it('should rowback the created todo item', () => {
+      cy.findByRole('textbox', {name: /New Todo/i}).type('New todo test');
+      cy.findByRole('button', {name: /Confirm creation of todo/i}).click();
+
+      cy.findByRole('checkbox', {name: /New todo test/i}).should('exist');
+      //awating the server response
+      cy.wait(300);
+      //rowback to "uncreated" state after the error message from server
+      cy.findByRole('checkbox', {name: /New todo test/i}).should('not.exist');
+      cy.findByText(/ERROR/i).should('exist');
+    });
+
+    it('should rowback the edit on the todo item', () => {
+      cy.findByRole('button', {name: /Test label 1 edit/i}).click();
+      //The {force: true} is because the input is been "covered" by the placeholder presentation
+      cy.findByRole('textbox', {name: /New description/i}).type('Edited todo label', {force: true});
+      cy.findByRole('button', {name: /Confirm change/i}).click();
+
+      cy.findByRole('checkbox', {name: /Test label 1/i, checked: true}).should('not.exist');
+      cy.findByRole('checkbox', {name: /Edited todo label/i, checked: true}).should('exist');
+      //awating the server response
+      cy.wait(300);
+      //rowback to "edited" state after the error message from server
+      cy.findByRole('checkbox', {name: /Test label 1/i, checked: true}).should('exist');
+      cy.findByRole('checkbox', {name: /Edited todo label/i, checked: true}).should('not.exist');
+      cy.findByText(/ERROR/i).should('exist');
+    });
+
+    it('should rowback removal of a todo item', () => {
+      cy.findByRole('button', {name: /Test label 2 removal/i}).click();
+
+      cy.findByRole('checkbox', {name: /Test label 2/i, checked: false}).should('not.exist');
+      //awating the server response
+      cy.wait(300);
+      //rowback to "uncreated" state after the error message from server
+      cy.findByRole('checkbox', {name: /Test label 2/i, checked: false}).should('exist');
+      cy.findByText(/ERROR/i).should('exist');
+    });
+
+    after(() => {
+      cy.mocksRestoreRouteVariants();
+    });
+  });
 })
